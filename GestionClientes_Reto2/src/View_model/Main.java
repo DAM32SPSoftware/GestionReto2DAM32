182da03ea6a53a619ccd461b4d29a895dd65e5cb
LAPTOP-VTECL76I\olatz <dam32.olatz.cano@gmail.com>
2023-01-18 19:49:37 +0100

    Diseño - Main Terminado *Falta quitar borde*

--- GestionClientes_Reto2/src/View_model/Main.java
+++ GestionClientes_Reto2/src/View_model/Main.java
@@ -2,9 +2,22 @@ package View_model;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Frame;
 
 public class Main extends JFrame {
 
@@ -23,6 +36,7 @@ public class Main extends JFrame {
 					e.printStackTrace();
 				}
 			}
+			
 		});
 	}
 
@@ -30,12 +44,93 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		//
+		Image imgIcon = Toolkit.getDefaultToolkit().getImage("resources/logoapp2.png");
+        setIconImage(imgIcon);
+		ImageIcon imgLogo = new ImageIcon("resources/logoapp2.png");
+		//
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 534, 634);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(45, 58, 58));
+		contentPane.setBorder(null);
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		//
+		setTitle("Gestor de Empleados");
+		setResizable(false);
+		setLocationRelativeTo(null);
+		//
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(new Color(4, 15, 15));
+		lblNewLabel.setIcon(new ImageIcon("resources/afterlife.png"));
+		lblNewLabel.setBounds(32, -23, 432, 235);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnEmpleados = new JButton("Listado de Empleados");
+		btnEmpleados.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		btnEmpleados.setBorder(null);
+		btnEmpleados.setBackground(new Color(251, 249, 255));
+		btnEmpleados.setBounds(137, 247, 231, 41);
+		contentPane.add(btnEmpleados);
+		
+		JLabel lblTeEncuentrasEn = new JLabel("Menú de empleados, escoge una opción:");
+		lblTeEncuentrasEn.setForeground(Color.WHITE);
+		lblTeEncuentrasEn.setFont(new Font("Trebuchet MS", Font.BOLD, 16));
+		lblTeEncuentrasEn.setBounds(89, 173, 330, 41);
+		contentPane.add(lblTeEncuentrasEn);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(22, 169, 468, 12);
+		contentPane.add(separator);
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setBounds(24, 212, 468, 12);
+		contentPane.add(separator_1);
+		
+		JButton btnOperacionesConEmpleados = new JButton("Operaciones Generales");
+		btnOperacionesConEmpleados.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		btnOperacionesConEmpleados.setBorder(null);
+		btnOperacionesConEmpleados.setBackground(new Color(251, 249, 255));
+		btnOperacionesConEmpleados.setBounds(137, 315, 231, 41);
+		contentPane.add(btnOperacionesConEmpleados);
+		
+		JButton btnHorarios = new JButton("Horarios");
+		btnHorarios.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		btnHorarios.setBorder(null);
+		btnHorarios.setBackground(new Color(251, 249, 255));
+		btnHorarios.setBounds(137, 384, 231, 41);
+		contentPane.add(btnHorarios);
+		
+		JButton btnBsquedas = new JButton("Búsquedas");
+		btnBsquedas.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		btnBsquedas.setBorder(null);
+		btnBsquedas.setBackground(new Color(251, 249, 255));
+		btnBsquedas.setBounds(137, 451, 231, 41);
+		contentPane.add(btnBsquedas);
+		
+		JButton btnSalir = new JButton("SALIR");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnSalir.setForeground(Color.WHITE);
+		btnSalir.setFont(new Font("Trebuchet MS", Font.BOLD, 18));
+		btnSalir.setBorder(null);
+		btnSalir.setBackground(new Color(166, 117, 161));
+		btnSalir.setBounds(184, 540, 140, 35);
+		contentPane.add(btnSalir);
+		
+		JSeparator separator_1_1 = new JSeparator();
+		separator_1_1.setBounds(22, 518, 468, 12);
+		contentPane.add(separator_1_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("resources/help2.png"));
+		lblNewLabel_1.setBounds(458, 540, 32, 35);
+		contentPane.add(lblNewLabel_1);
+		//
 	}
-
 }
